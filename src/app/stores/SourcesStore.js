db50fed4e3634781435ddcc20c1646e508c590ba
Baasbank Akinmuleya <baasb4real@yahoo.com>
2017-06-07 23:49:51 +0100

    Updates sourcesStore

--- src/app/stores/SourcesStore.js
+++ src/app/stores/SourcesStore.js
@@ -2,18 +2,24 @@
 /* eslint-env es6*/
 
 import { EventEmitter } from 'events';
-import axios from 'axios';
+// import axios from 'axios';
 
 class SourcesStore extends EventEmitter {
   constructor() {
     super();
-    this.sources = axios.get('https://newsapi.org/v1/sources?language=en')
-      .then((response) => {
-        console.log(response);
-      });
+    this.sources = [];
+    // this.getSources = this.getSources.bind(this);
   }
 }
+
+export const getSources = () => this.sources;
+
 
 const sourcesStore = new SourcesStore;
 
 export default SourcesStore;
+
+/* axios.get('https://newsapi.org/v1/sources?language=en')
+      .then((response) => {
+        console.log(response);
+      }); */
