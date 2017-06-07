1b917de6e4c7e8c2f3dfef81230d9e299803b798
Baasbank Akinmuleya <baasb4real@yahoo.com>
2017-06-07 08:58:28 +0100

    Create the sources store

---
+++ src/app/stores/SourcesStore.js
@@ -0,0 +1,19 @@
+/* eslint linebreak-style: ["error", "windows"]*/
+/* eslint-env es6*/
+
+import { EventEmitter } from 'events';
+import axios from 'axios';
+
+class SourcesStore extends EventEmitter {
+  constructor() {
+    super();
+    this.sources = axios.get('https://newsapi.org/v1/sources?language=en')
+      .then((response) => {
+        console.log(response);
+      });
+  }
+}
+
+const sourcesStore = new SourcesStore;
+
+export default SourcesStore;
