c69e0ec5e92dba2b05c15b6e62fe85e7d2467c39
Baasbank <baasb4real@yahoo.com>
2017-06-09 03:58:22 +0100

    Adds the receive sources action

---
+++ src/app/actions/Actions.js
@@ -0,0 +1,15 @@
+import Dispatcher from '../dispatcher/Dispatcher';
+// import * as ApiCalls from '../utilities/ApiCalls'
+import axios from 'axios';
+
+ export function getSourcesfromActions(){
+		return axios.get('https://newsapi.org/v1/sources?language=en')
+			.then(sources => Dispatcher.dispatch({
+				type: "RECIEVE_SOURCES",
+				sources: sources.data,
+			}))
+			// .catch(message => Dispatcher.dispatch({
+			// 	type: "RECIEVE_SOURCES_ERROR",
+			// 	message
+			// }));
+}
