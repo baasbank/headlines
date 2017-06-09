f989702e4aa71559166315cd54156830e6a45f03
Baasbank <baasb4real@yahoo.com>
2017-06-09 03:58:48 +0100

    Renders sources from store

--- src/app/components/Sources.js
+++ src/app/components/Sources.js
@@ -1,21 +1,63 @@
-/* eslint linebreak-style: ["error", "windows"]*/
+//  eslint linebreak-style: ["error", "windows"]
 /* eslint-env es6*/
 
 import React from 'react';
+import * as Actions from '../actions/Actions';
 
-import * as sourcesStore from '../stores/SourcesStore';
+import sourcesStore from '../stores/SourcesStore';
 
 export default class Sources extends React.Component {
   constructor() {
     super();
     this.state = {
-      sources: sourcesStore.getSources,
+       sources: [],
     };
   }
+
+  componentDidMount () {
+    Actions.getSourcesfromActions();
+    sourcesStore.on('change', () => (
+      this.setState({
+        sources: sourcesStore.getSources()
+      })
+    )
+  )
+}
+
   render() {
+     const  sources  = this.state.sources.sources;
+
+     const sourcesList = (sources === undefined) ? [] : sources;
+    //  console.log(sourcesList, "sources here in sources components");
+
+    //  let renderSources = function (){
+    //    return sources.map((source) => {
+    //     <li key={source.id}><a>{source.name}</a></li>;
+    //    })
+    //  }
+    // const sourceComponent =  sources.map((source) => {
+    //   return (
+    //    <li key={source.id}><a>{source.name}</a></li>
+    //   );
+    // })
+
     return (
       <div>
-        <h1>{'News Sources'}</h1>
+        <h1>News Sources</h1>
+        {/*<ul>{sourceComponent}</ul>*/}
+        <ul>
+          {
+            sourcesList.map((source) => {
+              return (
+                <div>
+                  <a href=''>
+                    <p key={source.index}>{source.name}</p>
+                  </a>
+                </div>
+              );
+            })
+          }
+        </ul>
       </div>
     );
   }
