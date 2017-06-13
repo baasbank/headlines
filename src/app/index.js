1811bed334e8c68fbae6c4e65c14f8d24b9e7767
Baasbank <baasb4real@yahoo.com>
2017-06-13 20:28:05 +0100

    update file

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { render } from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
 
@@ -6,6 +7,9 @@ import Sources from './components/Sources';
 import General from './components/General';
 import Headlines from './components/Headlines.jsx';
 import Login from './components/Login.jsx';
+import Navbar from './components/Navbar.jsx';
+
+import '../../sass/main.scss';
 
 const App = () => (
     <div>
@@ -14,6 +18,7 @@ const App = () => (
             <Route path="/articles/:article/:sortBy" component={Headlines}/>
             <Route path="/sources" component={Sources}/>
             <Route path="/login" component={Login}/>
+            <Route path="/navbar" component={Navbar}/>
           </Route>
         </Router>
   </div>
