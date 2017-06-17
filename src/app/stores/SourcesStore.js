ea32836debe73fffff0574ade420025e73a6991a
Baasbank <baasb4real@yahoo.com>
2017-06-17 18:29:57 +0100

    add JSDOC comments

--- src/app/stores/SourcesStore.js
+++ src/app/stores/SourcesStore.js
@@ -3,16 +3,38 @@ import Dispatcher from '../dispatcher/Dispatcher';
 // import axios from 'axios';
 
 class SourcesStore extends EventEmitter {
+/**
+   * Creates an instance of SourcesStore.
+   * @constructor
+   * @memberOf ArticlesStore
+   */
   constructor() {
     super();
+
+    /**
+     * Initializing the state of sources to an empty array,
+     * binding sources, message, and the getSources function.
+     */
+
     this.sources = [];
     this.message = '';
     this.getSources = this.getSources.bind(this);
   }
 
+  /**
+ * getSources function
+  * @return {array} news sources.
+ */
+
   getSources() {
     return this.sources;
   }
+
+/**
+     * the handleActions function handles the message dispatched by actions,
+     * and emits events and payload based on the type of action received.
+     * @param {object}
+*/
 
   handleActions(action) {
     if (action.type === 'RECIEVE_SOURCES') {
@@ -24,6 +46,8 @@ class SourcesStore extends EventEmitter {
     }
   }
 }
+
+
 const sourcesStore = new SourcesStore();
 Dispatcher.register(sourcesStore.handleActions.bind(sourcesStore));
 
