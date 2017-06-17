a5b2bac064e9554c96ceb8faab1a8d314bd90d08
Baasbank <baasb4real@yahoo.com>
2017-06-17 18:29:47 +0100

    add JSDOC comments

--- src/app/stores/ArticlesStore.js
+++ src/app/stores/ArticlesStore.js
@@ -4,15 +4,36 @@ import Dispatcher from '../dispatcher/Dispatcher';
 
 
 class ArticlesStore extends EventEmitter {
+  /**
+   * Creates an instance of ArticlesStore.
+   * @constructor 
+   */
   constructor() {
     super();
+
+    /**
+     * Initializing the state of articles to an empty array,
+     * and binding the getArticles function.
+     */
+
     this.articles = [];
     this.getArticles = this.getArticles.bind(this);
   }
 
+/**
+ * getArticles function
+  * @return {array} news articles.
+ */
+
   getArticles() {
     return this.articles;
   }
+
+/**
+     * the handleActions function handles the message dispatched by actions,
+     * and emits events and payload based on the type of action received.
+     * @param {object}
+*/
 
   handleActions(action) {
     if (action.type === 'RECEIVE_ARTICLES') {
